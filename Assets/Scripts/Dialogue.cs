775d295be139a102ce3b8b71a799e9451ea10f06
sebEFN <94054113+sebEFN@users.noreply.github.com>
2026-04-27 15:02:47 +0200

    textbox

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,8 +1,15 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
 {
-public TextMeshPro placeholder;
+public TextMeshProUGUI placeholder;
+
+ void Start()
+    {
+        placeholder.text = "HERE is my new message";
+    }
+
 }

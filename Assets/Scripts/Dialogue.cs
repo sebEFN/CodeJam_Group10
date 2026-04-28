89f4ddca4c98c105e34457dfcfad3db05b9cd2cd
sebEFN <94054113+sebEFN@users.noreply.github.com>
2026-04-28 14:34:11 +0200

    made dialogue work

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -10,6 +10,7 @@ public class Dialogue : MonoBehaviour
 {
 public TextMeshProUGUI placeholder;
 public TextEffect colorEffect;
+AudioSource audioData;
 private int i = 0;
 private int j = 0;
 private int k = 0;
@@ -19,51 +20,68 @@ private int t = 0;
  void Start()
         {
                 Debug.Log("start");
-               
+               audioData = GetComponent<AudioSource>();
         }
     
     
 public void MainGuydia()
     {
-            colorEffect.StartManualEffects();
-                if (i <= guy.mainGuy.Length)
+                if (i < guy.mainGuy.Length)
                 {
                         placeholder.text = guy.mainGuy[i];
                         i++;
                         Debug.Log(i);
+                        colorEffect.Refresh();
+            colorEffect.StartManualEffects();
                 }
+        else
+        {
+            Debug.Log("finished");
+        }
             
     }
     public void KaiDia()
     {
-            colorEffect.StartManualEffects();
-                if (j <= guy.kai.Length)
+        
+                if (j < guy.kai.Length)
                 {
                         placeholder.text = guy.kai[j];
-                        j++;
                         Debug.Log(j);
+                        colorEffect.Refresh();
+            colorEffect.StartManualEffects();
+            if (j == 1)
+            {
+                audioData.Play(0);
+            }
+            j++;
                 }
-            
+            else
+        {
+            Debug.Log("finished");
+        }
     }
 
     public void AnibaDia()
     {
-            colorEffect.StartManualEffects();
                 if (k <= guy.aniba.Length)
                 {
                         placeholder.text = guy.aniba[k];
                         k++;
+                        colorEffect.Refresh();
+            colorEffect.StartManualEffects();
                 }
             
     }
 
     public void AmalAI()
     {
-            colorEffect.StartManualEffects();
+
                 if (t <= guy.ai.Length)
                 {
                         placeholder.text = guy.ai[t];
                         t++;
+                                colorEffect.Refresh();
+            colorEffect.StartManualEffects();
                 }
             
     }

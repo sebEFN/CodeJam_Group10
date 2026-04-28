5b8b4e5bbad48ac47c32a3c95b02eff2b1a9e32f
Arkister <Harmanjeet2500@gmail.com>
2026-04-28 13:07:32 +0200

    Rhytmn system Part 1

--- Assets/Scripts/Rhythm.cs
+++ Assets/Scripts/Rhythm.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
 
 public class Rhythm : MonoBehaviour
 {
+    public GameObject MusicNotePrefab;
+    
     //The beats per minute the song has that we're trying to sync up to.
     public float SongBpm;
 
@@ -12,10 +16,12 @@ public class Rhythm : MonoBehaviour
     public float SongPosition;
 
     //the current position of the song, measured in beats. 
-    public float SongPostiionInBeats;
+    public float SongPositionInBeats;
 
     //how many seconds have passed since the song started.
     public float dspSongTime;
+
+    public float beatsShownInAdvance = 3f;
 
     //an AudioSource attached to this GameObject that will play the music. 
     public AudioSource musicSource;
@@ -25,6 +31,41 @@ public class Rhythm : MonoBehaviour
 
     //the index of the next note to be spawned
     int nextIndex = 0;
+
+    float offset = 0.05f;
+
+    float[] LoadOsuNotes(string path)
+    {
+        List<float> noteTimes = new List<float>();
+        string[] lines = File.ReadAllLines(path);
+
+        bool inHitObjects = false;
+
+        foreach (string line in lines)
+        {
+            if (line.StartsWith("[HitObjects]"))
+            {
+                inHitObjects = true;
+                continue;
+            }
+
+            if (inHitObjects)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                string[] parts = line.Split(',');
+
+                int timeMs = int.Parse(parts[2]);
+
+                float timeSec = timeMs / 1000f - offset;
+
+                noteTimes.Add(timeSec);
+            }
+        }
+
+        return noteTimes.ToArray();
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -36,7 +77,12 @@ public class Rhythm : MonoBehaviour
 
         //record the time when the music starts
         dspSongTime = (float)AudioSettings.dspTime;
-        Debug.Log(dspSongTime);
+
+        //Load Osu! beatmap into the notes array in seconds
+        string path = Path.Combine(Application.dataPath, "Resources/GameJamFinal.osu");
+        Debug.Log(path);
+        notes = LoadOsuNotes(path);
+        Debug.Log("Notes Loaded: " + notes.Length);
 
         //start the music
         musicSource.Play();
@@ -51,6 +97,14 @@ public class Rhythm : MonoBehaviour
         SongPosition = (float)(AudioSettings.dspTime - dspSongTime);
 
         //determine how many beats since the song started
-        SongPostiionInBeats = SongPosition/SecPerBeat;
+        SongPositionInBeats = SongPosition/SecPerBeat;
+
+        if (nextIndex < notes.Length && notes[nextIndex] < SongPosition + beatsShownInAdvance)
+            {
+                Instantiate(MusicNotePrefab, new Vector3(0f, 10f, 0f), Quaternion.identity);
+
+                //initialize the fields of the music note
+                nextIndex++;
+            }
     }
 }

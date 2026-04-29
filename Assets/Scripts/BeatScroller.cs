202e0828d20373d2e2632b85162c1bc6ba510905
Gialuan3 <gia-luan@hotmail.com>
2026-04-29 15:09:26 +0200

    game

---
+++ Assets/Scripts/BeatScroller.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class BeatScroller : MonoBehaviour
+{
+
+    public float beatTempo;
+
+    public bool hasStarted;
+
+    void Start()
+    {
+        beatTempo = beatTempo / 60f;
+    }
+
+ 
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            if(Input.anyKeyDown)
+            {
+                hasStarted = true;
+            }
+        }
+        else
+        {
+            transform.position -= new Vector3(0f, beatTempo * Time.deltaTime, 0f);
+        }
+    }
+}
---
+++ Assets/Scripts/NoteObject.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool canBePressed;
+
+    public KeyCode keyToPress;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(keyToPress))
+        {
+            if (canBePressed) 
+            { 
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Activator")
+        {
+            canBePressed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = false;
+        }
+    }
+}

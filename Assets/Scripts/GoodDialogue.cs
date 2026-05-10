dd27b35d166d45999c26b0b330888cf201c7d4be
sebEFN <94054113+sebEFN@users.noreply.github.com>
2026-05-10 23:32:38 +0200

    start

---
+++ Assets/Scripts/GoodDialogue.cs
@@ -0,0 +1,28 @@
+using EasyTextEffects;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoodDialogue : MonoBehaviour
+{
+    public TextMeshProUGUI placeholder;
+    public TextEffect colorEffect;
+    AudioSource audioData;
+    [SerializeField] DialogueData guy;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TheDialogue()
+    {
+        
+    }
+}

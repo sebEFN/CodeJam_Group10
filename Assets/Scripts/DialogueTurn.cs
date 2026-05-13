017e7c4f81a5e502fc6013a19d879f6ae95653ec
sebEFN <94054113+sebEFN@users.noreply.github.com>
2026-05-13 12:04:40 +0200

    ok

---
+++ Assets/Scripts/DialogueTurn.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueTurn
+{
+   public DialogueData character;
+   public int lineIndex;
+}
--- Assets/Scripts/GoodDialogue.cs
+++ Assets/Scripts/GoodDialogue.cs
@@ -1,30 +1,39 @@
 using EasyTextEffects;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Linq;
 using System.Collections;
 
 public class GoodDialogue : MonoBehaviour
 {
-    public TextMeshProUGUI DialogueText;
+    public TextMeshProUGUI dialogueText;
     public TextEffect colorEffect;
     public AudioSource talk;
+    public GameObject myGuy;
+    public DialogueTurn[] conversation;
     int who;
-    public DialogueData guy;
-
-    public void TheDialogue()
+    public void NextDialogue()
     {
-        if (who < 0 || who >= guy.mainGuy.Length)
+        who++;
+        Debug.Log(who);
+        if (who >= conversation.Length -2)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            
+            return;
         }
-        DialogueText.text = guy.mainGuy[who];
+        TheDialogue();
+    }
+    public void TheDialogue()
+    {
+        
+        DialogueTurn turn = conversation[who];
+        dialogueText.text = turn.character.mainGuy[turn.lineIndex];
+        Image img = myGuy.GetComponent<Image>();
+        img.sprite = turn.character.guyImage;
         colorEffect.Refresh();
         colorEffect.StartManualEffects();
         talk.Play();
-        who++;
-        
     }
 }

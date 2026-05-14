c9a08ec88909985040f7b4aa3200cfdb7456623f
donson-7 <kingmomoh240@gmail.com>
2026-05-14 14:00:35 +0200

    Changed it to support touch.

--- Assets/Scripts/Momohs Fix/Lane.cs
+++ Assets/Scripts/Momohs Fix/Lane.cs
@@ -8,18 +8,51 @@ using UnityEngine;
 public class Lane : MonoBehaviour
 {
     public Melanchall.DryWetMidi.MusicTheory.NoteName noteRestriction;
-    public KeyCode input;
+    bool tappedThisFrame = false;
     public GameObject notePrefab;
     List<Note> notes = new List<Note>();
     public List<double> timeStamps = new List<double>();
 
     int spawnIndex = 0;
     int inputIndex = 0;
+
     void Start()
     {
-        
+
     }
 
+    void TryHit()
+    {
+        if (inputIndex < timeStamps.Count)
+        {
+            double timeStamp = timeStamps[inputIndex];
+            double marginOfError = SongManager.Instance.marginOfError;
+            double audioTime = SongManager.GetAudioSourceTime() - (SongManager.Instance.inputDelayInMilliseconds / 1000.0);
+
+            if (Math.Abs(audioTime - timeStamp) < marginOfError)
+            {
+                Hit();
+                print($"Hit on {inputIndex} note");
+                if (inputIndex < notes.Count && notes[inputIndex] != null)
+                    Destroy(notes[inputIndex].gameObject);
+                inputIndex++;
+            }
+            else
+            {
+                print($"Hit inaccurate on {inputIndex} note wih {Math.Abs(audioTime - timeStamp)} delay");
+            }
+        }
+    }
+
+    public void OnTap()
+    {
+        if (!tappedThisFrame)
+        {
+            tappedThisFrame = true;
+            print("Lane tapped:" + gameObject.name);
+            TryHit();
+        }
+    }
     public void SetTimeStamps(Melanchall.DryWetMidi.Interaction.Note[] array)
     {
         foreach (var note in array)
@@ -31,9 +64,11 @@ public class Lane : MonoBehaviour
             }
         }
     }
-   
+
     void Update()
     {
+        tappedThisFrame = false;
+
         if (spawnIndex < timeStamps.Count)
         {
             if (SongManager.GetAudioSourceTime() >= timeStamps[spawnIndex] - SongManager.Instance.noteTime)
@@ -51,25 +86,27 @@ public class Lane : MonoBehaviour
             double marginOfError = SongManager.Instance.marginOfError;
             double audioTime = SongManager.GetAudioSourceTime() - (SongManager.Instance.inputDelayInMilliseconds / 1000.0);
 
-            if (Input.GetKeyDown(input))
-            {
-                if (Math.Abs(audioTime - timeStamp) < marginOfError)
-                {
-                    Hit();
-                    print($"Hit on {inputIndex} note");
-                    Destroy(notes[inputIndex].gameObject);
-                    inputIndex++;
-                }
-                else
-                {
-                    print($"Hit inaccurate on {inputIndex} note wih {Math.Abs(audioTime - timeStamp)} delay");
-                }
-            }
             if (timeStamp + marginOfError <= audioTime)
             {
                 Miss();
                 print($"Missed {inputIndex} note");
                 inputIndex++;
+            }
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector2 pos = Camera.main.ScreenToWorldPoint(touch.position);
+                RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+
+                if (hit.collider != null)
+                {
+                    hit.collider.GetComponentInParent<Lane>()?.OnTap();
+                }
             }
         }
     }
